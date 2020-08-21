b4c45b234eb84868a543ac49f840078353aaa48f
Ethan <eleetyson@gmail.com>
2020-08-21 12:56:35 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
     self.username.tr(" ", "-")
   end
 
-# input is test-123, output should be "test 123"
   def self.find_by_slug(slug)
     deslugged = slug.tr("-", " ")
     self.find_by(username: "#{deslugged}")
