f97af42231e8b4a9f8ef57aed2a45f58b352dae0
theadampalmer <theadampalmer@gmail.com>
2020-08-25 14:00:17 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,19 @@
 
 ActiveRecord::Schema.define(version: 2020_08_25_070053) do
 
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.text "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
