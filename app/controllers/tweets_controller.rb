eec89cf5c9237a93c74db75027a91e23261ca1df
theadampalmer <theadampalmer@gmail.com>
2020-08-25 14:44:42 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    # binding.pry
     if !logged_in?
       redirect '/login'
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,15 +19,16 @@ class UsersController < ApplicationController
    end
    
     get '/login' do
-    if !logged_in?
+      if !logged_in?
         erb :'users/login'
-    else
+      else
         redirect '/tweets'
+      end
     end
-    end
-
+    
     post '/login' do
-        @user = User.find_by(:username => params[:username])
+      @user = User.find_by(:username => params[:username])
+      # binding.pry
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect '/tweets'
